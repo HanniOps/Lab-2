3400b4fed68ffd121c50430143aed3d6a45608f2
Han Ni <hanniha16.22@ichat.sp.edu.sg>
2023-10-24 11:33:37 +0800

    Codes for Lab 2 exercises

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,36 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+
+
+def calc_average(numbers):
+    average = sum(numbers) / len(numbers)
+    print("The average is " + str(average))
+
+
+def get_user_input():
+    a = input()
+    y = a.split(", ")
+    numbers = list(map(float, y))  # converting str to float
+    return numbers
+
+
+def find_min_max(numbers):
+    minimum = min(numbers)
+    maximum = max(numbers)
+    print("Minimum number is " + str(minimum))
+    print("Maximum number is " + str(maximum))
+
+
+def sort_temperature():
+    print("sort_temperature")
+
+
+def calc_median_temperature():
+    print("calc_median_temperature")
+
+
+if __name__ == "__main__":
+    display_main_menu()
+    number = get_user_input()
+    calc_average(number)
+    find_min_max(number)
